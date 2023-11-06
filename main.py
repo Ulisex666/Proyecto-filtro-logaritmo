b38ed64dfbc4f686e34740b8bc3766f3208bb189
Sesilu00 <zS19014465@estudiantes.uv.mx>
2023-11-06 15:53:59 -0600

    Se agregó un botón que permite aplicar el filtro a la imagen seleccionada. Falta agregar elementos que permitan ajustar el parámetro "c".

--- main.py
+++ main.py
@@ -11,17 +11,24 @@ class VentanaPrincipal(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.img = QLabel(self)
         self.setWindowTitle("Transformación logaritmo")
         self.setFixedSize(QSize(1280, 720))
 
-        button_sel_img = QPushButton("Abrir imagen")
-        button_sel_img.setFixedSize(QSize(200, 100))
-        button_sel_img.clicked.connect(self.select_img_click)
+        self.img_original = QLabel(self)
+        self.ruta = None
+
+        boton_select_img = QPushButton("Abrir imagen")
+        boton_select_img.setFixedSize(QSize(200, 100))
+        boton_select_img.clicked.connect(self.select_img_click)
+
+        boton_filtro_log = QPushButton("Aplicar filtro")
+        boton_filtro_log.setFixedSize(QSize(200, 100))
+        boton_filtro_log.clicked.connect(self.filtro_img_click)
 
         h_layout = QHBoxLayout()
-        h_layout.addWidget(button_sel_img)
-        h_layout.addWidget(self.img)
+        h_layout.addWidget(boton_select_img)
+        h_layout.addWidget(boton_filtro_log)
+        h_layout.addWidget(self.img_original)
 
         contenedor = QWidget()
         contenedor.setLayout(h_layout)
@@ -29,12 +36,27 @@ class VentanaPrincipal(QMainWindow):
         self.setCentralWidget(contenedor)
         # self.setCentralWidget(button_sel_img)
 
-    def select_img_click(self, c):
-        img_name = QFileDialog.getOpenFileName(self, 'Open file', '/home')
-        orig_pix_map = QPixmap(img_name[0])
+    def select_img_click(self):
+        img_name = QFileDialog.getOpenFileName(self, 'Abrir archivo', '/home', '*.tif')
+        self.ruta = img_name[0]
+        orig_pix_map = QPixmap(self.ruta)
         scaled_pix_map = orig_pix_map.scaled(QSize(256, 256),
                                              Qt.AspectRatioMode.KeepAspectRatio)
-        self.img.setPixmap(scaled_pix_map)
+        self.img_original.setPixmap(scaled_pix_map)
+        self.img_original.setAlignment(Qt.AlignmentFlag.AlignTop)
+
+    def filtro_img_click(self):
+        if not self.ruta:
+            return 0
+        else:
+            img_no_filtro = cv2.imread(self.ruta)
+            img2 = img_no_filtro.astype(float)
+            img2 = 50 * np.log(1 + img_no_filtro)
+            img2 = img2.astype(np.uint8)
+            cv2.imshow('wey', img2)
+
+
+
 
 
 app = QApplication(sys.argv)
