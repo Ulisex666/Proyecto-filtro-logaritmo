d8b7bb32549e16e7a81e9ee5374e68d5a2e94aaa
Sesilu00 <zS19014465@estudiantes.uv.mx>
2023-11-08 21:27:52 -0600

    Se creó un layout para organizar los widgets de la forma deseada. Ahora también se pueden leer archivos .jpeg. Se agregó un label con instrucciones sobre el programa.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import (QApplication, QPushButton, QMainWindow, QFileDialog,
-                             QLabel, QWidget, QDoubleSpinBox,QHBoxLayout,
-                             QStyle, QVBoxLayout)
+                             QLabel, QWidget, QDoubleSpinBox, QGridLayout, QHBoxLayout,
+                             QStyle)
 from PyQt6.QtGui import QPixmap, QImage
 from PyQt6.QtCore import QSize, Qt
 import sys
@@ -29,7 +29,8 @@ class VentanaPrincipal(QMainWindow):
         boton_filtro_log.setFixedSize(QSize(100, 50))
         boton_filtro_log.clicked.connect(self.filtro_img_click)
 
-        boton_filtro_opt = QPushButton("Aplicar filtro")
+        boton_filtro_opt = QPushButton("Aplicar filtro \n"
+                                       "óptimo")
         boton_filtro_opt.setFixedSize(QSize(100, 50))
         boton_filtro_opt.clicked.connect(self.filtro_opt_click)
 
@@ -38,28 +39,45 @@ class VentanaPrincipal(QMainWindow):
         self.parametro.setPrefix('C=')
         self.parametro.setFixedSize(QSize(75, 20))
 
-        h_layout = QHBoxLayout()
-        h_layout.addWidget(boton_select_img)
-        h_layout.addWidget(boton_filtro_log)
-        h_layout.addWidget(boton_filtro_opt)
-        h_layout.addWidget(self.parametro)
-        h_layout.addWidget(self.img_original)
-        h_layout.addWidget(self.img_final)
+        tuto_label = QLabel('Selecciones una imagen para \n'
+                            'aplicar la transformación logaritmo. \n'
+                            'Mientras más aumente el factor C, \n'
+                            'aumentara el brillo de la imagen. \n'
+                            )
+        tuto_label.setFixedSize(QSize(200, 100))
+
+        layout_grid = QGridLayout()
+        layout_grid.setSpacing(20)
+        layout_botones = QGridLayout()
+        layout_botones.setSpacing(10)
+        layout_img = QGridLayout()
+
+        layout_botones.addWidget(boton_select_img, 0, 0)
+        layout_botones.addWidget(boton_filtro_log, 0, 1)
+        layout_botones.addWidget(boton_filtro_opt, 1, 0)
+        layout_botones.addWidget(self.parametro, 1, 1)
+        layout_botones.addWidget(tuto_label, 2, 0)
+
+        layout_img.addWidget(self.img_original, 0, 0)
+        layout_img.addWidget(self.img_final, 0, 1)
+
+        layout_grid.addLayout(layout_botones, 0, 0)
+        layout_grid.addLayout(layout_img, 0, 1)
 
         contenedor = QWidget()
-        contenedor.setLayout(h_layout)
+        contenedor.setLayout(layout_grid)
 
         self.setCentralWidget(contenedor)
 
     def select_img_click(self):
         img_name = QFileDialog.getOpenFileName(self, 'Abrir archivo', '/home/',
-                                               '*.tif *.jpg *.png')
+                                               '*.tif *.jpg *.png *.jpeg')
         self.ruta = img_name[0]
         orig_pix_map = QPixmap(self.ruta)
-        scaled_pix_map = orig_pix_map.scaled(QSize(256, 256),
+        scaled_pix_map = orig_pix_map.scaled(QSize(400, 400),
                                              Qt.AspectRatioMode.KeepAspectRatio)
         self.img_original.setPixmap(scaled_pix_map)
-        self.img_original.setAlignment(Qt.AlignmentFlag.AlignTop)
+        # self.img_original.setAlignment(Qt.AlignmentFlag.AlignTop)
 
     def filtro_img_click(self):
         if not self.ruta:
@@ -74,10 +92,10 @@ class VentanaPrincipal(QMainWindow):
             img_filtro = QImage(img_filtro, img_filtro.shape[1], img_filtro.shape[0],
                                 img_filtro.strides[0], QImage.Format.Format_Grayscale8)
             pix_map_filtro = QPixmap.fromImage(img_filtro)
-            scaled_pix_map_filtro = pix_map_filtro.scaled(QSize(256, 256),
+            scaled_pix_map_filtro = pix_map_filtro.scaled(QSize(400, 400),
                                                           Qt.AspectRatioMode.KeepAspectRatio)
             self.img_final.setPixmap(scaled_pix_map_filtro)
-            self.img_final.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            # self.img_final.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
     def filtro_opt_click(self):
         if not self.ruta:
@@ -87,15 +105,16 @@ class VentanaPrincipal(QMainWindow):
             img_no_filtro = cv2.cvtColor(img_no_filtro, cv2.COLOR_RGB2GRAY)
             img_no_filtro = img_no_filtro.astype(float)
             c = 255 / np.log(1 + np.max(img_no_filtro))
+            self.parametro.setValue(c)
             img_filtro = c * np.log(img_no_filtro + 1)
             img_filtro = img_filtro.astype(np.uint8)
             img_filtro = QImage(img_filtro, img_filtro.shape[1], img_filtro.shape[0],
                                 img_filtro.strides[0], QImage.Format.Format_Grayscale8)
             pix_map_filtro = QPixmap.fromImage(img_filtro)
-            scaled_pix_map_filtro = pix_map_filtro.scaled(QSize(256, 256),
+            scaled_pix_map_filtro = pix_map_filtro.scaled(QSize(400, 400),
                                                           Qt.AspectRatioMode.KeepAspectRatio)
             self.img_final.setPixmap(scaled_pix_map_filtro)
-            self.img_final.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            # self.img_final.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
 
 app = QApplication(sys.argv)
@@ -104,19 +123,3 @@ ventana = VentanaPrincipal()
 ventana.show()
 
 sys.exit(app.exec())
-# img = cv2.imread('luna.tif')
-# c = 100
-
-# cambiar tipo a "float"
-# img2 = img.astype(float)
-
-# transformación Logaritmo :  s = c log(1 + r)
-# img2 = c * np.log(1 + img2)
-
-# regresar tipo a uint8
-# img2 = img2.astype(np.uint8)
-
-# cv2.imshow('Original', img)
-# cv2.imshow('Log', img2)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
