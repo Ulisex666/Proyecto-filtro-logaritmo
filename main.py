f3cfa2cf0ae489404408cc2646366062922941eb
Sesilu00 <zS19014465@estudiantes.uv.mx>
2023-11-09 23:30:55 -0600

    Se añadió la funcionalidad de guardar la imagen transformada.

--- main.py
+++ main.py
@@ -19,11 +19,18 @@ class VentanaPrincipal(QMainWindow):
 
         self.img_original = QLabel(self)
         self.img_final = QLabel(self)
+        self.pix_map_filtro = QPixmap()
+
+        self.bandera_img_final = False
         self.ruta = None
 
         accion_abrir_file = QAction('Abrir imagen', self)
         accion_abrir_file.triggered.connect(self.select_img_click)
         accion_abrir_file.setShortcut(QKeySequence('Ctrl+A'))
+
+        accion_save_file = QAction('Guardar imagen transformada', self)
+        accion_save_file.triggered.connect(self.save_file)
+        accion_save_file.setShortcut(QKeySequence('Ctrl+S'))
 
         accion_clear_img_original = QAction('Borrar imagen original', self)
         accion_clear_img_original.triggered.connect(self.clear_img_original)
@@ -33,14 +40,13 @@ class VentanaPrincipal(QMainWindow):
         accion_clear_img_filtro.triggered.connect(self.clear_img_final)
         accion_clear_img_filtro.setShortcut(QKeySequence('Ctrl+Alt+D'))
 
-
         menu_bar = self.menuBar()
         menu_archivo = menu_bar.addMenu('&Archivo')
         menu_archivo.addSeparator()
         menu_archivo.addAction(accion_abrir_file)
+        menu_archivo.addAction(accion_save_file)
         menu_archivo.addAction(accion_clear_img_original)
         menu_archivo.addAction(accion_clear_img_filtro)
-
 
         boton_select_img = QPushButton("Abrir imagen")
         boton_select_img.setFixedSize(QSize(100, 50))
@@ -67,6 +73,9 @@ class VentanaPrincipal(QMainWindow):
                             )
         tuto_label.setFixedSize(QSize(200, 100))
 
+        self.error_label = QLabel()
+        self.error_label.setFixedSize(QSize(200, 50))
+
         layout_grid = QGridLayout()
         layout_grid.setSpacing(20)
         layout_botones = QGridLayout()
@@ -78,6 +87,7 @@ class VentanaPrincipal(QMainWindow):
         layout_botones.addWidget(boton_filtro_opt, 1, 0)
         layout_botones.addWidget(self.parametro, 1, 1)
         layout_botones.addWidget(tuto_label, 2, 0)
+        layout_botones.addWidget(self.error_label, 3, 0)
 
         layout_img.addWidget(self.img_original, 0, 0)
         layout_img.addWidget(self.img_final, 0, 1)
@@ -91,6 +101,8 @@ class VentanaPrincipal(QMainWindow):
         self.setCentralWidget(contenedor)
 
     def select_img_click(self):
+        self.error_label.clear()
+        self.error_label.setStyleSheet('background-color:none')
         img_name = QFileDialog.getOpenFileName(self, 'Abrir archivo', '/home/',
                                                '*.tif *.jpg *.png *.jpeg')
         self.ruta = img_name[0]
@@ -101,7 +113,9 @@ class VentanaPrincipal(QMainWindow):
 
     def filtro_img_click(self):
         if not self.ruta:
-            return 0
+            self.error_label.setStyleSheet('background-color:red; font: 15pt Times New Roman')
+            self.error_label.setText('¡No hay ninguna imagen \n'
+                                     ' para transformar!')
         else:
             img_no_filtro = cv2.imread(self.ruta)
             img_no_filtro = cv2.cvtColor(img_no_filtro, cv2.COLOR_RGB2GRAY)
@@ -111,14 +125,17 @@ class VentanaPrincipal(QMainWindow):
             img_filtro = img_filtro.astype(np.uint8)
             img_filtro = QImage(img_filtro, img_filtro.shape[1], img_filtro.shape[0],
                                 img_filtro.strides[0], QImage.Format.Format_Grayscale8)
-            pix_map_filtro = QPixmap.fromImage(img_filtro)
-            scaled_pix_map_filtro = pix_map_filtro.scaled(QSize(400, 400),
-                                                          Qt.AspectRatioMode.KeepAspectRatio)
-            self.img_final.setPixmap(scaled_pix_map_filtro)
+            self.pix_map_filtro = QPixmap.fromImage(img_filtro)
+            scaled_pix_map_log = self.pix_map_filtro.scaled(QSize(400, 400),
+                                                       Qt.AspectRatioMode.KeepAspectRatio)
+            self.img_final.setPixmap(scaled_pix_map_log)
+            self.bandera_img_final = True
 
     def filtro_opt_click(self):
         if not self.ruta:
-            return 0
+            self.error_label.setStyleSheet('background-color:red; font: 15pt Times New Roman')
+            self.error_label.setText('¡No hay ninguna imagen \n'
+                                     ' para transformar!')
         else:
             img_no_filtro = cv2.imread(self.ruta)
             img_no_filtro = cv2.cvtColor(img_no_filtro, cv2.COLOR_RGB2GRAY)
@@ -129,10 +146,24 @@ class VentanaPrincipal(QMainWindow):
             img_filtro = img_filtro.astype(np.uint8)
             img_filtro = QImage(img_filtro, img_filtro.shape[1], img_filtro.shape[0],
                                 img_filtro.strides[0], QImage.Format.Format_Grayscale8)
-            pix_map_filtro = QPixmap.fromImage(img_filtro)
-            scaled_pix_map_filtro = pix_map_filtro.scaled(QSize(400, 400),
-                                                          Qt.AspectRatioMode.KeepAspectRatio)
-            self.img_final.setPixmap(scaled_pix_map_filtro)
+            self.pix_map_filtro = QPixmap.fromImage(img_filtro)
+            scaled_pix_map_opt = self.pix_map_filtro.scaled(QSize(400, 400),
+                                                       Qt.AspectRatioMode.KeepAspectRatio)
+            self.img_final.setPixmap(scaled_pix_map_opt)
+            self.bandera_img_final = True
+
+    def save_file(self):
+        if self.bandera_img_final is False:
+            self.error_label.setStyleSheet('background-color:red; font: 15pt Times New Roman')
+            self.error_label.setText('¡No hay ninguna imagen \n'
+                                     ' para guardar!')
+        else:
+            self.error_label.clear()
+            self.error_label.setStyleSheet('background-color:none')
+            save_img = self.pix_map_filtro
+            save_name = QFileDialog.getSaveFileName(self, 'Guardar archivo', '', '*.jpg')
+            save_img.save(save_name[0], 'JPG')
+
 
     def clear_img_original(self):
         self.img_original.clear()
