467eaf926ed75b451c181dd56c1c1bf491e9c49e
Sesilu00 <zS19014465@estudiantes.uv.mx>
2023-11-10 03:37:41 -0600

    Se agregó la funcionalidad de ver la imagen transformada en una nueva ventana. A partir de ahora no se harán cambios de funcionalidad, solo limpieza de código.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from PyQt6.QtWidgets import (QApplication, QPushButton, QMainWindow, QFileDialog,
                              QLabel, QWidget, QDoubleSpinBox, QGridLayout, QHBoxLayout,
-                             QStyle, QStatusBar, QToolBar)
+                             QStyle, QToolBar, QMessageBox)
 from PyQt6.QtGui import QPixmap, QImage, QAction, QKeySequence, QIcon
 from PyQt6.QtCore import QSize, Qt
 import sys
 import cv2
 import numpy as np
+
+
+class VentanaFullScreen(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Imagen transformada')
+        self.setWindowIcon(QIcon('icons/image.png'))
+        self.setMaximumSize(QSize(1080, 720))
 
 
 class VentanaPrincipal(QMainWindow):
@@ -14,7 +22,7 @@ class VentanaPrincipal(QMainWindow):
 
         self.setWindowTitle("Transformación logaritmo")
         self.setFixedSize(QSize(1280, 720))
-        icon = self.style().standardIcon(QStyle.StandardPixmap.SP_MessageBoxCritical)
+        icon = self.style().standardIcon(QStyle.StandardPixmap.SP_DesktopIcon)
         self.setWindowIcon(icon)
 
         self.img_original = QLabel(self)
@@ -23,6 +31,7 @@ class VentanaPrincipal(QMainWindow):
 
         self.bandera_img_final = False
         self.ruta = None
+        self.img_fullscreen = None
 
         accion_abrir_file = QAction(QIcon('icons/folder-open-document.png'), 'Abrir imagen', self)
         accion_abrir_file.setToolTip('Ctrl + a')
@@ -31,25 +40,31 @@ class VentanaPrincipal(QMainWindow):
 
         accion_save_file = QAction(QIcon('icons/disk-black.png'), 'Guardar imagen transformada', self)
         accion_save_file.setToolTip('Ctrl + s')
-        accion_save_file.triggered.connect(self.save_file)
+        accion_save_file.triggered.connect(self.save_file_click)
         accion_save_file.setShortcut(QKeySequence('Ctrl+S'))
+
+        accion_img_fullscreen = QAction(QIcon('icons/image.png'), 'Mostrar imagen transformada', self)
+        accion_img_fullscreen.setToolTip('Ctrl + f')
+        # accion_img_fullscreen.setCheckable(True)
+        accion_img_fullscreen.triggered.connect(self.show_img_fullscreen_toggle)
+        accion_img_fullscreen.setShortcut(QKeySequence('Ctrl+F'))
 
         accion_clear_img_original = QAction(QIcon('icons/cross.png'), 'Borrar imagen original', self)
         accion_clear_img_original.setToolTip('Ctrl + d')
-        accion_clear_img_original.triggered.connect(self.clear_img_original)
+        accion_clear_img_original.triggered.connect(self.clear_img_original_click)
         accion_clear_img_original.setShortcut(QKeySequence('Ctrl+D'))
 
         accion_clear_img_filtro = QAction(QIcon('icons/cross-circle.png'),
                                           'Borrar imagen transformada', self)
         accion_clear_img_filtro.setToolTip('Ctrl+Alt+d')
-        accion_clear_img_filtro.triggered.connect(self.clear_img_final)
+        accion_clear_img_filtro.triggered.connect(self.clear_img_final_click)
         accion_clear_img_filtro.setShortcut(QKeySequence('Ctrl+Alt+D'))
 
         accion_clear_all = QAction(QIcon('icons/cross-circle-frame.png'), 'Borrar todo', self)
         accion_clear_all.setToolTip('Ctrl + Shift + d')
         accion_clear_all.setShortcut(QKeySequence('Ctrl+Shift+d'))
-        accion_clear_all.triggered.connect(self.clear_img_original)
-        accion_clear_all.triggered.connect(self.clear_img_final)
+        accion_clear_all.triggered.connect(self.clear_img_original_click)
+        accion_clear_all.triggered.connect(self.clear_img_final_click)
 
         toolbar = QToolBar('Barra de herramientas')
         toolbar.setIconSize(QSize(32, 32))
@@ -57,10 +72,10 @@ class VentanaPrincipal(QMainWindow):
         self.addToolBar(toolbar)
         toolbar.addAction(accion_abrir_file)
         toolbar.addAction(accion_save_file)
+        toolbar.addAction(accion_img_fullscreen)
         toolbar.addAction(accion_clear_img_original)
         toolbar.addAction(accion_clear_img_filtro)
         toolbar.addAction(accion_clear_all)
-
 
         boton_select_img = QPushButton("Abrir imagen")
         boton_select_img.setFixedSize(QSize(100, 50))
@@ -81,15 +96,12 @@ class VentanaPrincipal(QMainWindow):
         self.parametro.setValue(40)
         self.parametro.setFixedSize(QSize(75, 20))
 
-        tuto_label = QLabel('Selecciones una imagen para \n'
+        tuto_label = QLabel('Seleccione una imagen para \n'
                             'aplicar la transformación logaritmo. \n'
                             'Mientras más aumente el factor C, \n'
                             'aumentara el brillo de la imagen. \n'
                             )
         tuto_label.setFixedSize(QSize(200, 100))
-
-        self.error_label = QLabel()
-        self.error_label.setFixedSize(QSize(200, 50))
 
         layout_grid = QGridLayout()
         layout_grid.setSpacing(20)
@@ -102,7 +114,6 @@ class VentanaPrincipal(QMainWindow):
         layout_botones.addWidget(boton_filtro_opt, 1, 0)
         layout_botones.addWidget(self.parametro, 1, 1)
         layout_botones.addWidget(tuto_label, 2, 0)
-        layout_botones.addWidget(self.error_label, 3, 0)
 
         layout_img.addWidget(self.img_original, 0, 0)
         layout_img.addWidget(self.img_final, 0, 1)
@@ -116,8 +127,6 @@ class VentanaPrincipal(QMainWindow):
         self.setCentralWidget(contenedor)
 
     def select_img_click(self):
-        self.error_label.clear()
-        self.error_label.setStyleSheet('background-color:none')
         img_name = QFileDialog.getOpenFileName(self, 'Abrir archivo', '/home/',
                                                '*.tif *.jpg *.png *.jpeg')
         self.ruta = img_name[0]
@@ -128,9 +137,8 @@ class VentanaPrincipal(QMainWindow):
 
     def filtro_img_click(self):
         if not self.ruta:
-            self.error_label.setStyleSheet('background-color:red; font: 15pt Times New Roman')
-            self.error_label.setText('¡No hay ninguna imagen \n'
-                                     ' para transformar!')
+            QMessageBox.warning(self, 'Error', '¡No hay ninguna imagen para transformar!')
+
         else:
             img_no_filtro = cv2.imread(self.ruta)
             img_no_filtro = cv2.cvtColor(img_no_filtro, cv2.COLOR_RGB2GRAY)
@@ -142,15 +150,13 @@ class VentanaPrincipal(QMainWindow):
                                 img_filtro.strides[0], QImage.Format.Format_Grayscale8)
             self.pix_map_filtro = QPixmap.fromImage(img_filtro)
             scaled_pix_map_log = self.pix_map_filtro.scaled(QSize(400, 400),
-                                                       Qt.AspectRatioMode.KeepAspectRatio)
+                                                            Qt.AspectRatioMode.KeepAspectRatio)
             self.img_final.setPixmap(scaled_pix_map_log)
             self.bandera_img_final = True
 
     def filtro_opt_click(self):
         if not self.ruta:
-            self.error_label.setStyleSheet('background-color:red; font: 15pt Times New Roman')
-            self.error_label.setText('¡No hay ninguna imagen \n'
-                                     ' para transformar!')
+            QMessageBox.warning(self, 'Error', '¡No hay ninguna imagen para transformar!')
         else:
             img_no_filtro = cv2.imread(self.ruta)
             img_no_filtro = cv2.cvtColor(img_no_filtro, cv2.COLOR_RGB2GRAY)
@@ -163,27 +169,34 @@ class VentanaPrincipal(QMainWindow):
                                 img_filtro.strides[0], QImage.Format.Format_Grayscale8)
             self.pix_map_filtro = QPixmap.fromImage(img_filtro)
             scaled_pix_map_opt = self.pix_map_filtro.scaled(QSize(400, 400),
-                                                       Qt.AspectRatioMode.KeepAspectRatio)
+                                                            Qt.AspectRatioMode.KeepAspectRatio)
             self.img_final.setPixmap(scaled_pix_map_opt)
             self.bandera_img_final = True
 
-    def save_file(self):
+    def save_file_click(self):
         if self.bandera_img_final is False:
-            self.error_label.setStyleSheet('background-color:red; font: 15pt Times New Roman')
-            self.error_label.setText('¡No hay ninguna imagen \n'
-                                     ' para guardar!')
+            QMessageBox.warning(self, 'Error', '¡No hay ninguna imagen para guardar!')
         else:
-            self.error_label.clear()
-            self.error_label.setStyleSheet('background-color:none')
             save_img = self.pix_map_filtro
             save_name = QFileDialog.getSaveFileName(self, 'Guardar archivo', '', '*.jpg')
             save_img.save(save_name[0], 'JPG')
 
+    def show_img_fullscreen_toggle(self):
+        if not self.bandera_img_final:
+            QMessageBox.warning(self, 'Error', '¡No hay ninguna imagen para mostrar!')
+        else:
+            self.img_fullscreen = VentanaFullScreen()
+            img_fullscreen_label = QLabel(self)
+            img_fullscreen_label.setPixmap(self.pix_map_filtro)
+            img_fullscreen_label.setMaximumSize(QSize(1080, 720))
+            self.img_fullscreen.setCentralWidget(img_fullscreen_label)
+            self.img_fullscreen.show()
 
-    def clear_img_original(self):
+    def clear_img_original_click(self):
         self.img_original.clear()
+        self.ruta = None
 
-    def clear_img_final(self):
+    def clear_img_final_click(self):
         self.img_final.clear()
         self.bandera_img_final = False
 
